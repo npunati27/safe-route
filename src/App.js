9b9700588839583e48344fa586d24d08e7d184c9
Ananya Anand <ananyaanand@wirelessprv-10-194-87-75.near.illinois.edu>
2023-02-24 22:32:16 -0600

    reading csv file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Papa from 'papaparse';
+import { useState } from 'react';
+
 function App() {
+  const [csvData, setCsvData] = useState([Daily_Crime_Log.csv]);
+
+  const handleData = (results) => {
+    setCsvData(results.data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +18,20 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input
+          type="file"
+          onChange={(e) => {
+            Papa.parse(e.target.files[0], {
+              complete: handleData,
+              header: true,
+            });
+          }}
+        />
+        <ul>
+          {csvData.map((row, i) => (
+            <li key={i}>{JSON.stringify(row)}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
